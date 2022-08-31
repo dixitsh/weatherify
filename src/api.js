e7a5354329ba6c85376fa4e1b28f1705621e71a7
Ayush <ayushkvf@gmail.com>
2022-08-31 22:43:00 +0530

    completed App

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7aa1a165b7mshfb13952e5ddc642p108c09jsn955b1570917b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY="d31e308e0afb5071a1c8c43596278a0f"
